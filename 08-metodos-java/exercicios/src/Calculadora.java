60e565e3b83268d8f09598db1dd51aa72fd5d627
Brenno Machado <brenno.pm@gmail.com>
2022-03-03 22:16:52 -0300

    inicio exercios parte 1 da etapa 8 de métodos me java

---
+++ 08-metodos-java/exercicios/src/Calculadora.java
@@ -0,0 +1,26 @@
+public class calculadora {
+
+    public static double soma(double numero1, double numero2){
+        double resultado = numero1 + numero2;
+        System.out.println(numero1  + " + " + numero2 + " = " + resultado);
+        return resultado;
+    }
+
+    public static double subtracao(double numero1, double numero2){
+        double resultado = numero1 - numero2;
+        System.out.println(numero1  + " - " + numero2 + " = " + resultado);
+        return resultado;
+    }
+
+    public static double multiplicacao(double numero1, double numero2){
+        double resultado = numero1 * numero2;
+        System.out.println(numero1  + " * " + numero2 + " = " + resultado);
+        return resultado;
+    }
+
+    public static double divisao(double numero1, double numero2){
+        double resultado = numero1 / numero2;
+        System.out.println(numero1  + " / " + numero2 + " = " + resultado);
+        return resultado;
+    }
+}
